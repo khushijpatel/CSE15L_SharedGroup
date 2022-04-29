a21a139a5dad3b392b2c20eee25962cd3302dbb6
khushijpatel <103156629+khushijpatel@users.noreply.github.com>
2022-04-29 13:09:35 -0700

    fixed failing test 5

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -74,7 +74,7 @@ public class MarkdownParseTest {
         String content = Files.readString(file);
         ArrayList<String> links = MarkdownParse.getLinks(content);
         ArrayList<String> result = new ArrayList<String>(
-            Arrays.asList("some-thing.html"));
+            Arrays.asList("some-thing.html", "https://google.com", "https://google.com"));
 
         assertEquals(true, links.equals(result));
     }
