12f7cdb55e288a0165c65d38b3e3e91fd176cd7e
jypipi <63512723+jypipi@users.noreply.github.com>
2022-04-30 01:56:35 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -15,7 +15,7 @@ public class MarkdownParseTest {
     @Test
     public void checkTestFile() throws IOException {
         // test-file.md
-        Path file = Path.of("C:\\Users\\Jeffrey Chen\\Documents\\GitHub\\CSE15L_SharedGroup\\test-file.md");
+        Path file = Path.of("test-file.md");
         String content = Files.readString(file);
         ArrayList<String> links = MarkdownParse.getLinks(content);
         ArrayList<String> result = new ArrayList<String>() {
@@ -31,7 +31,7 @@ public class MarkdownParseTest {
     @Test
     public void checkTest2() throws IOException {
         // test2.md
-        Path file = Path.of("C:\\Users\\Jeffrey Chen\\Documents\\GitHub\\CSE15L_SharedGroup\\test2.md");
+        Path file = Path.of("test2.md");
         String content = Files.readString(file);
         ArrayList<String> links = MarkdownParse.getLinks(content);
         ArrayList<String> result = new ArrayList<String>(
@@ -46,7 +46,7 @@ public class MarkdownParseTest {
     @Test
     public void checkTest3() throws IOException {
         // test3.md
-        Path file = Path.of("C:\\Users\\Jeffrey Chen\\Documents\\GitHub\\CSE15L_SharedGroup\\test3.md");
+        Path file = Path.of("test3.md");
         String content = Files.readString(file);
         ArrayList<String> links = MarkdownParse.getLinks(content);
         ArrayList<String> result = new ArrayList<String>(
@@ -58,7 +58,7 @@ public class MarkdownParseTest {
     @Test
     public void checkTest4() throws IOException {
         // test4.md
-        Path file = Path.of("C:\\Users\\Jeffrey Chen\\Documents\\GitHub\\CSE15L_SharedGroup\\test4.md");
+        Path file = Path.of("test4.md");
         String content = Files.readString(file);
         ArrayList<String> links = MarkdownParse.getLinks(content);
         ArrayList<String> result = new ArrayList<String>(
@@ -70,7 +70,7 @@ public class MarkdownParseTest {
     @Test
     public void checkTest5() throws IOException {
         // Lab 5 Part 2
-        Path file = Path.of("C:\\Users\\Jeffrey Chen\\Documents\\GitHub\\CSE15L_SharedGroup\\test1.md");
+        Path file = Path.of("test1.md");
         String content = Files.readString(file);
         ArrayList<String> links = MarkdownParse.getLinks(content);
         ArrayList<String> result = new ArrayList<String>(
@@ -83,7 +83,7 @@ public class MarkdownParseTest {
     @Test
     public void checkTest6() throws IOException {
         // test5.md
-        Path file = Path.of("C:\\Users\\Jeffrey Chen\\Documents\\GitHub\\CSE15L_SharedGroup\\test5.md");
+        Path file = Path.of("test5.md");
         String content = Files.readString(file);
         ArrayList<String> links = MarkdownParse.getLinks(content);
         ArrayList<String> result = new ArrayList<String>(
